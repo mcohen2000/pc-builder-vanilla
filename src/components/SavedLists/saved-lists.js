f3ee2109d44c08d6321bad4960e31fd6da8eb1c4
Michael Cohen <macball2000@yahoo.com>
2022-09-10 01:51:10 -0400

    JS: Saved Lists Check If Selection is Empty

--- src/components/SavedLists/saved-lists.js
+++ src/components/SavedLists/saved-lists.js
@@ -22,31 +22,55 @@ function loadLists() {
       listName.innerText = data.name;
       const listPrice = document.createElement("p");
       listPrice.classList += "listPrice";
-      listPrice.innerText = `$${data["total-price"]} + tax`;
+      listPrice.innerText = `Total Price: $${data["total-price"]} + tax`;
       const listCpu = document.createElement("p");
       listCpu.classList += "list-part";
       listCpu.innerText = `CPU: ${data.cpu.manufacturer} ${data.cpu.name} - $${data.cpu.price}`;
+      if (data.cpu === ""){
+        listCpu.innerText = `CPU: Empty - $0.00`;
+      }
       const listCpuCooler = document.createElement("p");
       listCpuCooler.classList += "list-part";
       listCpuCooler.innerText = `CPU Cooler: ${data["cpu-cooler"].manufacturer} ${data["cpu-cooler"].name} - $${data["cpu-cooler"].price}`;
+      if (data["cpu-cooler"] === ""){
+        listCpuCooler.innerText = `CPU Cooler: Empty - $0.00`;
+      }
       const listMotherboard = document.createElement("p");
       listMotherboard.classList += "list-part";
       listMotherboard.innerText = `Motherboard: ${data.motherboard.manufacturer} ${data.motherboard.series} ${data.motherboard.name} ${data.motherboard.socket} - $${data.motherboard.price}`;
+      if (data.motherboard === ""){
+        listMotherboard.innerText = `Motherboard: Empty - $0.00`;
+      }
       const listGpu = document.createElement("p");
       listGpu.classList += "list-part";
-      listGpu.innerText = `GPU: ${data.gpu.manufacturer} ${data.gpu.chipset} ${data.gpu.memory} ${data.gpu.name} - $${data.gpu.price}`;
+      listGpu.innerText = `Video Card: ${data.gpu.manufacturer} ${data.gpu.chipset} ${data.gpu.memory} ${data.gpu.name} - $${data.gpu.price}`;
+      if (data.gpu === ""){
+        listGpu.innerText = `Video Card: Empty - $0.00`;
+      }
       const listRam = document.createElement("p");
       listRam.classList += "list-part";
       listRam.innerText = `Memory: ${data.ram.manufacturer} ${data.ram.name} ${data.ram["memory-config"]} ${data.ram.type}-${data.ram.speed} - $${data.ram.price}`;
+      if (data.ram === ""){
+        listRam.innerText = `Memory: Empty - $0.00`;
+      }
       const listStorage = document.createElement("p");
       listStorage.classList += "list-part";
       listStorage.innerText = `Storage: ${data.storage.manufacturer} ${data.storage.name} ${data.storage.size} ${data.storage.type} - $${data.storage.price}`;
+      if (data.storage === ""){
+        listStorage.innerText = `Storage: Empty - $0.00`;
+      }
       const listPsu = document.createElement("p");
       listPsu.classList += "list-part";
       listPsu.innerText = `Power Supply: ${data.psu.manufacturer} ${data.psu.name} ${data.psu.wattage}W ${data.psu.rating} - $${data.psu.price}`;
+      if (data.psu === ""){
+        listPsu.innerText = `Power Supply: Empty - $0.00`;
+      }
       const listCase = document.createElement("p");
       listCase.classList += "list-part";
       listCase.innerText = `Case: ${data.case.manufacturer} ${data.case.name} ${data.case.type} - $${data.case.price}`;
+      if (data.case === ""){
+        listCase.innerText = `Case: Empty - $0.00`;
+      }
       newCard.append(listName);
       newCard.append(listCpu);
       newCard.append(listCpuCooler);
